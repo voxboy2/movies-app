73f2bd49834365d21675f43953657de1a0d1655a
voxboy2 <ebieroma323@gmail.com>
2020-07-23 09:14:21 +0100

    almost-last

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -5,5 +5,5 @@ export const USER_SERVER = '/api/users';
 export const API_KEY = 'e055512d08933beea063b266913fd4e6'
 export const API_URL = 'https://api.themoviedb.org/3/';
 
-export const IMAGE_URL = 'http://image.tmdb.org/t/p/';
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/';
 
